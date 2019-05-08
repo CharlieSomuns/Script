f4f070217df06f86267e08fe2cd94b1f507cb76e
CharlieSomuns <39690449+CharlieSomuns@users.noreply.github.com>
2019-05-08 17:54:16 +0800

    Add files via upload

---
+++ install_docker_ce.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce -y
+
+sudo gpasswd -a ${USER} docker
+
+sudo newgrp docker
+
+sudo mkdir -p /data/docker
+
+sed -i -r 's/^(ExecStart=\/usr\/bin\/dockerd)(.*?)/\1\2 \-g \/data\/docker/' /lib/systemd/system/docker.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl restart docker
+
+sudo systemctl enable docker
+
+#https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1
